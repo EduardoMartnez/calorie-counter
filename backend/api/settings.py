996fef7337efea914fc08d15d0b5a139381dde8a
Eduardo Martinez <feduka2000@gmail.com>
2026-04-10 19:48:36 -0400

    [Implementation] Dockerize the project and used nginx for a reverse-proxy

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -143,14 +143,12 @@ CORS_ALLOW_ALL_ORIGINS=True # Disable in production
 CORS_ALLOW_CREDENTIALS=True
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.{}'.format(
-             os.getenv('DATABASE_ENGINE', 'sqlite3')
-         ),
-         'NAME': os.getenv('DATABASE_NAME', 'polls'),
-         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
-         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
-         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
-         'PORT': os.getenv('DATABASE_PORT', 5432),
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
--- frontend/utils/auth.tsx
+++ frontend/utils/auth.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/users/";
+const API_URL = "/api/users/";
 
 export const registerUser = async (email: string, username: string, password: string) => {
     try {
