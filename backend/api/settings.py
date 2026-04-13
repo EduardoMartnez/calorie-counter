61e2efa0ee0947780be0091e8e0a10cb9e475db4
Eduardo Martinez <feduka2000@gmail.com>
2026-04-13 14:42:14 -0400

    [Implementation] Created app for handling food data

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",default="django-unsecure-5(ub4!+%0kd4a7l-@)tq7*anc*rbmn7ulgf0f^f#!t1cy4gstd")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=0))
