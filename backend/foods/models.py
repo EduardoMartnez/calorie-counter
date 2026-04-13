43a46809157ea8099adbba5b23be039ded57fc77
Eduardo Martinez <feduka2000@gmail.com>
2026-04-13 15:01:47 -0400

    [Implementation] Added models for the Foods app

--- backend/foods/models.py
+++ backend/foods/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class FoodCategories(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    category_description = models.CharField()
+
+class Foods(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    food_description = models.CharField()
+    food_category_id = models.ForeignKey(FoodCategories)
+
+class FoodNutrients(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField()
+
+class FoodNutrients(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    food_id = models.ForeignKey(Foods)
+    nutrient_id = models.ForeignKey(FoodNutrients)
+    amount = models.IntegerField()
+
+class FoodPortion(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    food_id = models.ForeignKey(Foods)
+    seq_num = models.SmallIntegerField()
+    portion_description = models.CharField()
+    gram_weight = models.IntegerField()
